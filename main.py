f3eb3a5646a44cc31e4cd3afd0cf4b97e93db2c3
unaveenj <unaveenj@gmail.com>
2020-12-10 13:20:44 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+import sys
+
+if __name__ =="__main__":
+    pygame.init()
+
+    display_surface = pygame.display.set_mode((650,650)) #initialise game window
+    clock = pygame.time.Clock()
+    test_surface = pygame.Surface((200,200))
+    test_surface.fill((121, 216, 224))
+    test_rect = test_surface.get_rect(center = (325,325))
+    #display_surface.fill((152, 235, 52)) #Lime green background
+    #Define a event loop
+    while True :
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT :
+                pygame.quit()
+                sys.exit()
+        display_surface.fill((175,215,70))
+        display_surface.blit(test_surface,test_rect) #blit-> block image transfer
+        pygame.display.update()
+        clock.tick(60) #executes the loop at 60FPS
+
+
+
+
+
